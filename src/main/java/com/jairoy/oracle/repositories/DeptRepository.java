fb65858f558f624c6d99db08876a308b87da61bc
jairoy <jairo.yenecura@gmail.com>
2023-11-12 17:43:55 -0300

    DeptRepository

---
+++ src/main/java/com/jairoy/oracle/repositories/DeptRepository.java
@@ -0,0 +1,9 @@
+package com.jairoy.oracle.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jairoy.oracle.entities.Dept;
+
+public interface DeptRepository extends JpaRepository<Dept, Integer>{
+
+}
